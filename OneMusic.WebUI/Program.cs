33d84773442a0ab96efabe335f80156816a9fd5a
forvisualstudio40@hotmail.com <forvisualstudio40@hotmail.com>
2024-04-30 22:11:48 +0300

    Add project files.

---
+++ OneMusic.WebUI/Program.cs
@@ -0,0 +1,44 @@
+using OneMusic.BusinessLayer.Abstract;
+using OneMusic.BusinessLayer.Concrete;
+using OneMusic.DataAccessLayer.Abstract;
+using OneMusic.DataAccessLayer.Concrete;
+using OneMusic.DataAccessLayer.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddScoped<IAboutDal,EfAboutDal>();
+builder.Services.AddScoped<IAboutService,AboutManager>();
+
+builder.Services.AddScoped<IAlbumDal, EfAlbumDal>();
+builder.Services.AddScoped<IAlbumService, AlbumManager>();
+
+builder.Services.AddScoped<IBannerDal, EfBannerDal>();
+builder.Services.AddScoped<IBannerService, BannerManager>();
+
+builder.Services.AddDbContext<OneMusicContext>();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
